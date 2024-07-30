5f79bf79684f64ca17f73e6792ebd9fc3a3274fc
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-30 09:42:51 +0300

    Tender

---
+++ custom_addons/tender_management/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Tender Management ',
+    'version': '1.0.0',
+    # 'category': '',
+    'author': 'Mark',
+    'sequence': 50,
+    'summary': 'Tender Management system',
+    'description': """ Tender Management system """,
+    'depends': ['mail', 'product'],
+    'data': [
+        'data/ir_sequence_data.xml',
+        "security/ir.model.access.csv",
+        "views/menu.xml",
+        "views/tender_views.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
