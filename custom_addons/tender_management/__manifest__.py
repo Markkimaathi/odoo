5974b62335d3e8d0f61104a078f8dc6c5d262184
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-31 11:55:09 +0300

    Security

--- custom_addons/tender_management/__manifest__.py
+++ custom_addons/tender_management/__manifest__.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 
-
 {
-    'name': 'Tender Management ',
+    'name': 'Tender Management',
     'version': '1.0.0',
-    # 'category': '',
-    'author': 'Mark',
+    'author': 'Joshua',
     'sequence': 50,
     'summary': 'Tender Management system',
-    'description': """ Tender Management system """,
+    'description': """Tender Management system""",
     'depends': ['mail', 'product'],
     'data': [
         'data/ir_sequence_data.xml',
-        "security/ir.model.access.csv",
-        "views/menu.xml",
-        "views/tender_views.xml",
+        'security/ir.model.access.csv',
+        'security/tender_management_security.xml',
+        'views/menu.xml',
+        'views/tender_views.xml',
     ],
     'demo': [],
     'installable': True,
--- custom_addons/tender_management/models/tender.py
+++ custom_addons/tender_management/models/tender.py
@@ -1,11 +1,11 @@
 from odoo import api, fields, models
-from _datetime import date
+from datetime import date
 
 
 class TenderManagement(models.Model):
     _name = "tender.management"
     _description = "Tender Management"
-    _inherit = ['mail.thread', 'mail.activity.mixin',]
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Many2one('res.users', string="Purchase Representative")
     ref = fields.Char(string="Reference", readonly=True, copy=False, default='New')
@@ -20,20 +20,23 @@ class TenderManagement(models.Model):
         ('done', 'DONE'),
         ('cancel', 'CANCEL')], string='State', default='draft', required=True
     )
-    days_to_deadline= fields.Integer(string='Days To Deadline', compute='_compute_days')
+    days_to_deadline = fields.Integer(string='Days To Deadline', compute='_compute_days')
     bid_ids = fields.One2many('tender.bid', 'tender_id', string="Bids")
     bid_count = fields.Integer(string='Bid Count', compute='_compute_bid_count')
-    tender_management_line_ids=fields.One2many('tender.management.line','tender_management_id',string='Tender Management Line')
+    tender_management_line_ids = fields.One2many('tender.management.line', 'tender_management_id', string='Tender Management Line')
     formatted_date = fields.Char(string='Formatted Date', compute='_compute_formatted_date')
+    category=fields.Char(string='Category')
+    rank=fields.Char(string='Top Rank')
 
     @api.depends('date_created')
     def _compute_formatted_date(self):
         for record in self:
             if record.date_created:
-                date = fields.Date.from_string(record.date_created)
-                record.formatted_date = f''' {date.strftime("%d")} \n {date.strftime("%b %Y")} '''
+                date_obj = fields.Date.from_string(record.date_created)
+                record.formatted_date = f''' {date_obj.strftime("%d")} \n {date_obj.strftime("%b %Y")} '''
             else:
                 record.formatted_date = ''
+
     @api.depends('date_bid_to_end')
     def _compute_days(self):
         for rec in self:
@@ -43,25 +46,32 @@ class TenderManagement(models.Model):
                 rec.days_to_deadline = days_difference
             else:
                 rec.days_to_deadline = 0
+
     @api.depends('bid_ids')
     def _compute_bid_count(self):
         for tender in self:
             tender.bid_count = len(tender.bid_ids)
+
     def action_approve(self):
-       for rec in self:
-           rec.state = 'approve'
+        for rec in self:
+            rec.state = 'approve'
+
     def action_done(self):
-       for rec in self:
-           rec.state = 'done'
+        for rec in self:
+            rec.state = 'done'
+
     def action_cancel(self):
         for rec in self:
             rec.state = 'cancel'
+
     def action_draft(self):
         for rec in self:
             rec.state = 'draft'
+
     def action_approved(self):
         for rec in self:
             rec.state = 'approved'
+
     def action_submit(self):
         for rec in self:
             rec.state = 'submit'
@@ -72,12 +82,26 @@ class TenderManagement(models.Model):
             vals['ref'] = self.env['ir.sequence'].next_by_code('tender.management') or 'New'
         return super(TenderManagement, self).create(vals)
 
-    class TenderManagementLine(models.Model):
-        _name='tender.management.line'
-        _description = "Tender Management Line"
 
-        product_id = fields.Many2one('product.product', string='Products')
-        price_unit = fields.Float(string='Price')
-        qty = fields.Integer(string='Quantity')
-        tender_management_id=fields.Many2one('tender.management',string='Tender Management')
+class TenderManagementLine(models.Model):
+    _name = 'tender.management.line'
+    _description = "Tender Management Line"
 
+    product_id = fields.Many2one('product.product', string='Products')
+    product_uom_id = fields.Many2one(
+        comodel_name='uom.uom',
+        string='Product Uom',
+        compute='_compute_product_uom_id', store=True, readonly=False, precompute=True,
+        ondelete="restrict",
+    )
+    product_quantity = fields.Many2one(
+        comodel_name='uom.uom',
+        string='Product Quantity',
+        compute='_compute_product_uom_id', store=True, readonly=False, precompute=True,
+        ondelete="restrict",
+    )
+    price_unit = fields.Float(string='Price', related='product_id.list_price')
+    qty = fields.Integer(string='Quantity')
+    default_code = fields.Char(related='product_id.default_code', string='Code')
+    name = fields.Char(string='Description')
+    tender_management_id = fields.Many2one('tender.management', string='Tender Management')
