f4912962ebe2add73d7bfc4b6a94c11b8c82d516
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-08-02 16:18:59 +0300

    Bid

--- custom_addons/tender_management/models/bid.py
+++ custom_addons/tender_management/models/bid.py
@@ -1,12 +1,37 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
 
+class Bid(models.Model):
+    _name = 'tender.bid'
+    _description = 'Bid'
 
+    def _get_default_user(self):
+        return self.env.user.id
 
-class TenderBid(models.Model):
-    _name = "tender.bid"
-    _description = "Tender Bid"
+    name = fields.Many2one('res.users', string="Purchase Representative", default=_get_default_user)
+    tender_id = fields.Many2one('tender.management', string="Tender", required=True)
+    ref = fields.Char(string="Reference", copy=False, default='New', readonly=True)
+    partner_id = fields.Many2many('res.partner', string="Vendor")
+    date_created = fields.Date(string='Start Date', default=fields.Date.context_today)
+    date_bid_to_end = fields.Date(string='End Date', default=fields.Date.context_today)
+    bid_amount = fields.Float(string="Bid Amount")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('submitted', 'Submitted'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ], string='Status', readonly=True, copy=False, index=True, default='draft')
 
-    name = fields.Char(string="Name")
-    bid_ref = fields.Char(string="Bid Reference", copy=False, default='New', readonly=True)
-    bid_count=fields.Many2one('tender', string='Bids')
-    tender_id = fields.Many2one('tender.management', string="Tender", ondelete='cascade')
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('tender.bid') or _('New')
+        return super(Bid, self).create(vals)
+
+    def action_submit(self):
+        self.write({'state': 'submitted'})
+
+    def action_accept(self):
+        self.write({'state': 'accepted'})
+
+    def action_reject(self):
+        self.write({'state': 'rejected'})
